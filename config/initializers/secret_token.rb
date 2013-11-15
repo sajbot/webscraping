f3504fedcf275f26c7a5ab674bf73b8ff6bcba26
sajbot <tobias.boman@outlook.com>
2013-11-15 07:05:33 +0100

    2nd commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webscraping::Application.config.secret_key_base = '50155573532d2ba35a140bc3f2653562b70a0dddae3ed104788fc312c098c00d0aff426928236cb07d4d464bf232e6acf90aed1796d2ee8f3b9ad7d5200fde3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webscraping::Application.config.session_store :cookie_store, key: '_Webscraping_session'
